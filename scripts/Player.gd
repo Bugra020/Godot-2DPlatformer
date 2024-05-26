ff5191ad9c60d55a6d98e0a3a3a1846dfa419db2
Kerem Adalı <kerem.adali14@gmail.com>
2024-05-26 19:27:46 +0300

    jumping mechanics improved

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -15,8 +15,13 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump")and(  is_on_floor()or _animation.animation=="landing"):
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("jump"):
+		if on_floor:
+			velocity.y = JUMP_VELOCITY
+		elif (_animation.animation=="landing"):
+			velocity.y = JUMP_VELOCITY
+		elif (_animation.animation=="idle" or _animation.animation=="walking"):
+			velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
