a5b6f362624b8d7bd942f9039fe85faa1d1f2d34
bugra <bugrakaya020@gmail.com>
2024-05-26 21:30:55 +0300

    attacking anim added

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,6 +6,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var on_floor
 var landed
 var jump_progress
+var attacking = false
 @onready var _animation = $AnimatedSprite2D
 
 func _physics_process(delta):
@@ -45,12 +46,14 @@ func _process(delta):
 		landed = true
 	if _animation.animation =="jumping" and _animation.frame_progress == 1 and not on_floor:
 		_animation.frame = 10
-		
+	if _animation.animation == "melee_attack" and _animation.frame == 8:
+		attacking = false
+	if _animation.animation != "melee_attack":
+		attacking = false
 		
 	if left and on_floor:
 		_animation.play("walking")
 	elif right and on_floor:
-		
 		_animation.play("walking")
 	elif Input.is_action_pressed("jump") and not on_floor:
 		_animation.play("jumping")
@@ -58,11 +61,15 @@ func _process(delta):
 	elif not on_floor:
 		_animation.play("jumping")
 		landed = false
-	elif on_floor and landed==false:
+	elif on_floor and landed==false and not attacking:
 		_animation.play("landing")
 	elif on_floor and landed:
-		_animation.play("idle")
-	elif right and left:
+		if Input.is_action_pressed("melee_attack") and not attacking:
+			_animation.play("melee_attack")
+			attacking = true;
+		elif not attacking:
+			_animation.play("idle")
+	elif right and left and not attacking:
 		_animation.play("idle")
 
 
